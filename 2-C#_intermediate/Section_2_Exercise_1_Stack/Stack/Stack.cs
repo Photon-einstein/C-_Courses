bfad7d2bc4c419f6bd906a98d0c711618a35314c
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2023-03-27 23:26:05 +0100

    Add files via upload

---
+++ 2-C#_intermediate/Section_2_Exercise_1_Stack/Stack/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack
+    {
+        private List<object> _stack;
+        private int _sizeStack = 0;
+
+        public Stack()
+        {
+            _stack = new List<object>();
+            if (_stack == null)
+            {
+                throw new InvalidOperationException("Stack memory allocation failed.");
+            }
+        }
+
+        public void Push(object item)
+        {
+            if (item == null)
+            {
+                throw new InvalidOperationException("\nException1: Item cannot be null!\n");
+            }
+            _stack.Add(item);
+            ++_sizeStack;
+            return;
+        }
+        public object Pop()
+        {
+            if (_sizeStack == 0)
+            {
+                throw new InvalidOperationException("\nException 2: Can't do a pop from an empty stack.\n");
+            }
+            object o = _stack[_sizeStack-1];
+            _stack.RemoveAt(_sizeStack - 1);
+            --_sizeStack;
+            return o;
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+            return;
+        }
+    }
+}
