19a08c8f4e064f84054e42fcac1509329c9cdf5d
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2023-03-30 00:02:05 +0100

    Add files via upload

---
+++ 2-C#_intermediate/Section_3_Exercise_1_DatabaseConnection/DataBaseConnection/OracleConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseConnection
+{
+    internal class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connectionString) : base(connectionString)
+        {
+        }
+
+        public OracleConnection(string connectionString, TimeSpan timeOutSpan) : base(connectionString, timeOutSpan)
+        {
+        }
+
+        public override bool OpenConnection()
+        {
+            Console.WriteLine("Class 'OracleConnection' openning database connection.");
+            return true;
+        }
+
+        public override bool CloseConnection()
+        {
+            Console.WriteLine("Class 'OracleConnection' closing database connection.");
+            return true;
+        }
+    }
+}
