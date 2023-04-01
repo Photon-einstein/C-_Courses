45df76a3dbec27a882c93d0a6f581bb767a4ec57
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2023-04-01 01:56:08 +0100

    Add files via upload

---
+++ 2-C#_intermediate/Section_3_Exercise_2_DatabaseCommand/DataBaseConnection/SqlConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseConnection
+{
+    internal class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString) 
+        {
+        }
+
+        public SqlConnection(string connectionString, TimeSpan timeOutSpan) : base(connectionString, timeOutSpan)    
+        {
+        }
+
+        public override bool OpenConnection()
+        {
+            Console.WriteLine("\nClass 'SqlConnection' openning database connection.");
+            return true;
+        }
+
+        public override bool CloseConnection()
+        {
+            Console.WriteLine("Class 'SqlConnection' closing database connection.");
+            return true;
+        }
+    }
+}
