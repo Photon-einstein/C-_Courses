94bcdcb149894af6045b4c2e334cccc95a6ac4c1
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2023-04-02 02:51:31 +0100

    Add files via upload

---
+++ 2-C#_intermediate/Section_4_Exercise_1_WorkflowEngine/WorkflowEngine/WorkflowEngine/CallWebService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowEngine
+{
+    internal class CallWebService : IActivity
+    {
+        public CallWebService() { 
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("# 'CallWebService' object: executing");
+        }
+    }
+}
